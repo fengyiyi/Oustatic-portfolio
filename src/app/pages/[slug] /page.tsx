50137170f09275cd3eb553a92b5a177088e31fb8
Fred <fengyiyi@users.noreply.github.com>
2023-09-25 15:55:13 +0800

    Update page.tsx

--- src/app/pages/[slug] /page.tsx
+++ src/app/pages/[slug] /page.tsx
@@ -1,127 +1,67 @@
-import Image from 'next/image'
-import { Metadata } from 'next'
-import { OstDocument } from 'outstatic'
-import Header from '@/components/Header'
-import Layout from '@/components/Layout'
-import markdownToHtml from '@/lib/markdownToHtml'
-import { getDocumentSlugs, load } from 'outstatic/server'
-import DateFormatter from '@/components/DateFormatter'
-import { absoluteUrl } from '@/lib/utils'
-
-type Post = {
-  tags: { value: string; label: string }[]
-} & OstDocument
-
-interface Params {
-  params: {
-    slug: string
-  }
-}
-
-export async function generateMetadata(params: Params): Promise<Metadata> {
-  const post = await getData(params)
-
-  if (!post) {
-    return {}
-  }
-
-  return {
-    title: post.title,
-    description: post.description,
-    openGraph: {
-      title: post.title,
-      description: post.description,
-      type: 'article',
-      url: absoluteUrl(`/pages/${post.slug}`),
-      images: [
-        {
-          url: absoluteUrl(post?.coverImage || '/images/og-image.png'),
-          width: 1200,
-          height: 630,
-          alt: post.title
-        }
-      ]
-    },
-    twitter: {
-      card: 'summary_large_image',
-      title: post.title,
-      description: post.description,
-      images: absoluteUrl(post?.coverImage || '/images/og-image.png')
-    }
-  }
-}
-
-export default async function Post(params: Params) {
-  const post = await getData(params)
-  return (
-    <Layout>
-      <div className="max-w-6xl mx-auto px-5">
-        <Header />
-        <article className="mb-32">
-          <div className="relative mb-2 md:mb-4 sm:mx-0 w-full h-52 md:h-96">
-            <Image
-              alt={post.title}
-              src={post?.coverImage || ''}
-              fill
-              className="object-cover object-center"
-              priority
-            />
-          </div>
-          {Array.isArray(post?.tags)
-            ? post.tags.map(({ label }) => (
-                <span
-                  key="label"
-                  className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-                >
-                  {label}
-                </span>
-              ))
-            : null}
-          <h1 className="font-primary text-2xl font-bold md:text-4xl mb-2">
-            {post.title}
-          </h1>
-          <div className="hidden md:block md:mb-12 text-slate-600">
-            Written on <DateFormatter dateString={post.publishedAt} /> by{' '}
-            {post?.author?.name || ''}.
-          </div>
-          <hr className="border-neutral-200 mt-10 mb-10" />
-          <div className="max-w-2xl mx-auto">
-            <div
-              className="prose lg:prose-xl"
-              dangerouslySetInnerHTML={{ __html: post.content }}
-            />
-          </div>
-        </article>
-      </div>
-    </Layout>
-  )
-}
-
-async function getData({ params }: Params) {
-  const db = await load()
-
-  const post = await db
-    .find<Post>({ collection: 'pages', slug: params.slug }, [
-      'title',
-      'publishedAt',
-      'description',
-      'slug',
-      'author',
-      'content',
-      'coverImage',
-      'tags'
-    ])
-    .first()
-
-  const content = await markdownToHtml(post.content)
-
-  return {
-    ...post,
-    content
-  }
-}
-
+import { getDocumentBySlug, getDocuments } from "outstatic/server";
+import React from "react";
+import { Project } from "@/types";
+import markdownToHtml from "@/lib/markdownToHtml";
+import Image from "next/image";
+import { Button } from "@/components";
+import { DrukWide } from "@/app/fonts";
+// Return a list of `params` to populate the [slug] dynamic segment
 export async function generateStaticParams() {
-  const posts = getDocumentSlugs('pages')
-  return posts.map((slug) => ({ slug }))
+  const allPosts = (await getDocuments("projects", [
+    "title",
+    "publishedAt",
+    "slug",
+    "coverImage",
+    "description",
+  ])) as unknown as Project[];
+
+  return allPosts.map((post: Project) => ({
+    slug: post.slug,
+  }));
 }
+
+async function Projectpage({ params }: { params: { slug: string } }) {
+  const { slug } = params;
+  const project = getDocumentBySlug("projects", slug, [
+    "title",
+    "publishedAt",
+    "description",
+    "slug",
+    "author",
+    "content",
+    "coverImage",
+  ]) as unknown as Project;
+  const content = await markdownToHtml(project.content || "");
+  return (
+    <>
+      <div className="flex flex-col gap-8 mt-8">
+        <div className="flex my-8">
+          <Button href="/" variant="ghost">
+            <Image
+              src="/icons/Back.svg"
+              alt={project.description}
+              width={24}
+              height={24}
+            />
+            Back
+          </Button>
+        </div>
+      </div>
+
+      <div className=" flex flex-col gap-8 mt-20 mx-auto max-w-3xl">
+        <h1
+          className="text-3xl font-bold mb-4 md:text-6xl"
+          style={DrukWide.style}
+        >
+          {project.title}
+        </h1>
+        <p>{project.description}</p>
+        <div dangerouslySetInnerHTML={{ __html: content }} />
+      </div>
+    </>
+  );
+}
+
+export default Projectpage;
+
+export const revalidate = "force-cache";
